e1f44c9d543a16169aa4204884ea40ea0ef37719
87abhinav.bhatnagar <kinabhinav>
2017-07-06 09:30:31 -0300

    First commit

---
+++ src/test/java/com/blackpearl/blackpearl/LoginTest.java
@@ -0,0 +1,22 @@
+package com.blackpearl.blackpearl;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LoginTest { 
+	public WebDriver driver;
+	@Test
+	public void login()
+	
+	{
+	
+	System.setProperty("webdriver.chrome.driver", "/Users/abhatnagar/eclipse/jee-oxygen2/Eclipse.app/Contents/MacOS/Applications/blackpearl/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "/usr/local/share/chromedriver");
+	driver = new ChromeDriver();
+		
+	driver.get("https://blackpearl.kinductdev.com/login");
+	}
+}
